0cd718cf0e9be248d947f377af28a0ba4a70b19c
KismetLalligithub <lalli.kismet@gmail.com>
2025-06-24 22:23:57 -0700

    Deployed the library on pypi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             ],
         classifiers=[
             "Programming Language :: Python :: 3", 
-            "License :: OSI Approved :: MIT Licesnse", 
+            "License :: OSI Approved :: MIT License", 
             "Operating System :: OS Independent",
             ],
         python_requires='>=3.8',
